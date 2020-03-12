40792081501fbda333c10829290d5e1d4fe8d01c
kligan <45855643+kligan@users.noreply.github.com>
2020-03-12 20:51:17 +0000

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,13 +7,13 @@ import React, { Component } from 'react';
 let quizData = require('./quiz_data.json')
 
  class Quiz extends Component{
-    constructor(props){
-        super(props)
-this.state = {quiz_position: 1};
+    
+    
+
 
 
       
-    }
+  
     render(){
         return(
     <div>
