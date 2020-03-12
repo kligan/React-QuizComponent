96e0042c2f85da119fa49d9cfbb72a1bdb1e5d0c
kligan <45855643+kligan@users.noreply.github.com>
2020-03-12 20:25:19 +0000

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,4 +6,8 @@ import React, { Component } from 'react';
 
 let quizData = require('./quiz_data.json')
 
-
+export default class Quiz extends Component{
+    constructor(props){
+        super(props)
+    }
+}
